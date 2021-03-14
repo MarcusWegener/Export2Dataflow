b6591bc7e6a001bfea6636921132f87f4d1c392b
Marcus Wegener <39794269+MarcusWegener@users.noreply.github.com>
2021-03-14 14:19:16 +0100

    Added files to create the External Tool in Power BI Desktop

---
+++ Export2Dataflow.ps1
@@ -0,0 +1,247 @@
+﻿<# 
+.SYNOPSIS
+	This script exports the Power Query to a Dataflow JSON file
+.DESCRIPTION
+	This Powershell script can be called as an External Tool from Power BI Desktop, which then extracts the Power Queries via the 
+	TOM (Tabular Object Model) and stores them in the Dataflow JSON format.
+.NOTES
+    File Name	: Export2Dataflow.ps1
+	Date		: 03/14/2021
+	Version		: 1.0.0 
+    Author		: Marcus Wegener
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+	OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+.LINK 
+	https://github.com/MarcusWegener/Export2Dataflow
+.Parameter Server
+	The server name and portnumber of the local instance of Analysis Services Tabular for imported/DirectQuery data models
+.Parameter Database
+	The database name of the model hosted in the local instance of Analysis Services Tabular for imported/DirectQuery data models
+#>
+
+
+# Below section defines the server and database based on the input captured from the External tools integration 
+# This is defined as arguments \"%server%\" and \"%database%\" in the external tools json 
+Param(
+	[string]$server,
+	[string]$database
+)
+
+# Write Intro, Server and Database information to screen
+Write-Host -ForegroundColor White  '========================================================================================================================'
+Write-Host -ForegroundColor Yellow '                ______                           __  ___    ____          __          ____ __                           '
+Write-Host -ForegroundColor Yellow '               / ____/_  __ ____   ____   _____ / /_|__ \  / __ \ ____ _ / /_ ____ _ / __// /____  _      __            '
+Write-Host -ForegroundColor Yellow '              / __/  | |/_// __ \ / __ \ / ___// __/__/ / / / / // __ `// __// __ `// /_ / // __ \| | /| / /            '
+Write-Host -ForegroundColor Yellow '             / /___ _>  < / /_/ // /_/ // /   / /_ / __/ / /_/ // /_/ // /_ / /_/ // __// // /_/ /| |/ |/ /             '
+Write-Host -ForegroundColor Yellow '            /_____//_/|_|/ .___/ \____//_/    \__//____//_____/ \__,_/ \__/ \__,_//_/  /_/ \____/ |__/|__/              '
+Write-Host -ForegroundColor Yellow '                        /_/                                                                                             '
+Write-Host -ForegroundColor Yellow '                                                                          By Marcus Wegener  v1.0.0.0   03/14/2021      '
+Write-Host -ForegroundColor White  '========================================================================================================================'
+Write-Host -ForegroundColor White  "Your Power BI Model currently runs with the following connection details:"
+Write-Host -ForegroundColor White  "Server: " $server 
+Write-Host -ForegroundColor White  "Database: " $database 
+
+# Install latest package (if not already installed) of Microsoft.AnalysisServices.retail.amd64 for current user
+Write-Host -ForegroundColor White  '========================================================================================================================'
+Write-Host -ForegroundColor Gray  "Install latest package (if not already installed) of Microsoft.AnalysisServices.retail.amd64 for current user..."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-Package Microsoft.AnalysisServices.retail.amd64 -Scope CurrentUser -Source "https://www.nuget.org/api/v2"
+$installedPackages = Find-Package -Name Microsoft.AnalysisServices.retail.amd64 -Source "$env:USERPROFILE\AppData\Local\PackageManagement\NuGet\Packages\" -AllVersions
+$maxVersion = $installedPackages[0].Version
+
+# Load Assembly Files from insalled package Microsoft.AnalysisServices.retail.amd64
+Write-Host -ForegroundColor Gray "Load Assembly Files from insalled package Microsoft.AnalysisServices.retail.amd64..."
+$assemblyPathTabular = "$env:USERPROFILE\AppData\Local\PackageManagement\NuGet\Packages\Microsoft.AnalysisServices.retail.amd64.$maxVersion\lib\net45\Microsoft.AnalysisServices.Tabular.dll"
+
+
+
+Write-Host -ForegroundColor White  '========================================================================================================================'
+
+# Connect to Power BI Model
+Write-Host -ForegroundColor Gray "Connect to Power BI Model..."
+
+Add-Type -Path $assemblyPathTabular
+
+$as = New-Object Microsoft.AnalysisServices.Tabular.Server
+$as.Connect($server)
+$db = $as.Databases[$database]
+$dbModel = $db.Model
+
+$modelCulture = $dbModel.Culture
+$modelModifiedTime = Get-Date($dbModel.ModifiedTime) -format s
+
+# Create query groups
+Write-Host -ForegroundColor Gray "`Create query groups..."
+
+$queryGroups = @{}
+foreach($queryGroup in $dbModel.QueryGroups) {
+		$groupStructur = $queryGroup.Folder.Split("\")
+		$groupName = $groupStructur[-1]
+		$groupParentName = ""
+
+		if($groupStructur.Length -gt 1) {
+			$groupParentName = $groupStructur[0..($groupStructur.Length -2)] -join "\"
+		}
+		
+        $queryGroups[$queryGroup.Name] = [ordered]@{
+			"id" = New-Guid
+			"name" = $groupName
+			"description" = $dbModel.QueryGroups[0].Annotations.description
+			"parentId" = $queryGroups[$groupParentName].id
+			"order" = $groupStructur.Length
+		}
+}
+
+$pbiQueryGroups = ""
+
+if($queryGroups.Count -le 1) {
+    $pbiQueryGroups = "["
+}
+
+$pbiQueryGroups += $queryGroups.values | ConvertTo-Json -Compress 
+
+if($queryGroups.Count -le 1) {
+    $pbiQueryGroups += "]"
+}
+
+
+# Create queries
+Write-Host -ForegroundColor Gray "`nCreate queries..."
+
+$queriesMetadata = @{}
+$entities = @()
+$document = "section Section1;`n"
+
+
+foreach($e in $dbModel.Expressions) {
+    $queriesMetadata[$e.Name] =  [ordered]@{ 
+		queryId = New-Guid
+		queryName = $e.Name
+    }
+    if($e.QueryGroup) {
+        $queriesMetadata[$e.Name].queryGroupId = $queryGroups[$e.QueryGroup.Folder].id
+    }
+    
+    $tableName = $e.Name
+    if($tableName -notmatch '^[a-z0-9]+$') {
+        $tableName = "#"""+$tableName+""""
+    }
+
+    $document += "shared " + $tableName + " = " + $e.Expression + ";`n"
+}
+
+foreach($t in $dbModel.Tables) {
+    if($t.Partitions.SourceType -eq "M") {
+        $queriesMetadata[$t.Name] = [ordered]@{ 
+			queryId = New-Guid
+			queryName = $t.Name
+			"loadEnabled" = $true
+		}
+        if($t.Partitions.QueryGroup) {
+			$queriesMetadata[$t.Name].queryGroupId = $queryGroups[$t.Partitions.QueryGroup.Folder].id
+        }
+
+        $tableName = $t.Name
+        if($tableName -notmatch '^[a-z0-9]+$') {
+          $tableName = "#"""+$tableName+""""
+        }
+
+        $document += "shared " + $tableName + " = " + $t.Partitions.Source.Expression + ";`n"
+
+        $attributes = @()
+
+        foreach($c in $t.Columns) {
+			if ($c.Type -eq "Data") {
+               $attributes += [ordered]@{ 
+					name = $c.Name 
+					dataType = $c.DataType.ToString().ToLower()
+				}
+			}
+        }
+
+        $entities += [ordered]@{ 
+			'$type' = "LocalEntity"
+			"name" = $t.Name
+			"description" = $t.Description
+			"pbi:refreshPolicy"= [ordered]@{
+				'$type' = "FullRefreshPolicy"
+				"location" = [uri]::EscapeDataString($t.Name) + ".csv"
+			}
+			"attributes" = $attributes
+        }
+    }
+}
+
+$document = $document.Replace("`n","`r`n")
+
+Write-Host -ForegroundColor Gray "`nDisconnect from Power BI Model..."
+
+$as.Disconnect();
+
+# Save file dialog
+Write-Host -ForegroundColor Gray "Export Dataflow model to file..."
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$fileBrowser = New-Object System.Windows.Forms.SaveFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'JSON Files (*.json)|*.json'
+    Title = 'Export Dataflow model to file:'
+}
+
+$null = $fileBrowser.ShowDialog()
+$fileName = $fileBrowser.FileName
+
+if(!$fileName){
+    Write-Host -ForegroundColor Yellow "No file chosen. Terminating..."
+    Start-Sleep -Seconds 1.5
+    exit
+}
+
+$fileNameWithoutPath = Split-path $fileBrowser.FileName -leaf
+
+
+Write-Host -ForegroundColor Gray "$fileName chosen..."
+
+Write-Host -ForegroundColor White  '========================================================================================================================'
+
+$dataflowName = [regex]::Match($fileNameWithoutPath, ".+?(?=\.json)").Value
+
+$json = [ordered]@{ 
+	"name" = $dataflowName
+	"description" = ""
+	"version" = "1.0"
+	"culture" = $modelCulture
+	"modifiedTime" = $modelModifiedTime
+	"pbi:mashup" = [ordered]@{
+		"fastCombine" = $false
+		"allowNativeQueries" = $false
+		"skipAutomaticHeaderAndTypeDetection" = $false
+		"queriesMetadata" = $queriesMetadata
+		"document" = $document
+	}
+	"annotations" = @(
+		[ordered]@{
+			"name" = "pbi:QueryGroups"
+			"value" = $pbiQueryGroups
+		}
+	)
+	"entities" = $entities
+ }
+
+Write-Host -ForegroundColor Gray "`nWrite into JSON file..."
+
+$json | ConvertTo-Json -Depth 10 -Compress | Out-File -Encoding UTF8 "$fileName"
+
+Start-Process -FilePath C:\Windows\explorer.exe -ArgumentList "/select, ""$fileName"""
+
+
+Write-Host -ForegroundColor Gray "Dataflow model file ($fileName) created..."
+Write-Host -ForegroundColor White  '========================================================================================================================'
+Start-Sleep -Seconds 1.5
--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # Export2Dataflow
-This repository contains everything needed to run the Export2Dataflow Powershell script as an External Tool in the Power BI Desktop and export the Power Query to a Dataflow JSON file.
+This repository contains everything needed to run the Export2Dataflow PowerShell script as an External Tool in the Power BI Desktop and export the Power Query to a Dataflow JSON file.
+
+## Disclaimer
+Please know, that everything I have created and shared on the blogpost and on GitHub is based on best effort. No rights can be derived, as well as I am not liable for the use or misuse of the solution or possible damage resulting from this. Use of the solutions and execution of the scripts is all on your own risk and your own responsibility.
+
+## Getting Started
+### Download everything you need from this repository.
+* External Tool integration file: _export2dataflow.pbitool.json_
+* The PowerShell script: _Export2Dataflow.ps1_
+
+### Copy the External Tool integration file
+The External Tool integration file is needed to get the button in the Power BI ribbon. In order to achieve this, you need to copy the _export2dataflow.pbitool.json_ file in the External Tools folder. For me this location was:
+_C:\Program Files (x86)\Common Files\Microsoft Shared\Power BI Desktop\External Tools_
+
+While copying the files, it can be that Windows asks you to login with admin privileges before you can continue. This is mandatory to copy the files. If you cannot do this yourself, please contact your administrator.
+
+### Copy the PowerShell script
+Create a subfolder _Export2Dataflow_ in the _C:\Program Files\_ folder and copy the PowerShell script _Export2Dataflow.ps1_ file into it.
+
+Identical to the previous step, Windows may ask you to authenticate with administrator privileges before you can proceed.
+
+### Restart Power BI Desktop
+You have applied all required steps by now. A new button will appear in the Power BI Desktop top ribbon for External Tools. In case you had Power BI running already, please restart Power BI desktop first.
+
+Did something not workout as expected for you, kindly check the FAQ to see if your question is already listed there. If not, please let me know.
+
+## Usage
+
+
+
+## Author
+Marcus Wegener 
+
+[Website](https://thinkbi.de) - 
+[twitter](https://twitter.com/PowerBIler) - 
+[LinkedIn](https://www.linkedin.com/in/marcuswegener/) - 
+[Xing](https://www.xing.com/profile/Marcus_Wegener3/cv)
+
+## Acknowledgements
+Marc Lelijveld, for his tutorial on creating external tools for Power BI and his GitHub repository [External-Tools-Model-Documentation](https://github.com/marclelijveld/External-Tools-Model-Documentation), from which I was able to adopt a lot.
+
+[Website](https://data-marc.com/) - 
+[Blogpost](https://data-marc.com/2020/07/28/external-tools-document-your-power-bi-model/) - 
+[twitter](https://twitter.com/PowerBIler) - 
+[LinkedIn](https://www.linkedin.com/in/marclelijveld/) - 
+[Github](https://github.com/marclelijveld/External-Tools-Model-Documentation)
+
+Julian Kaiser, for reviewing, testing and adding some features to the script.
+
+[LinkedIn](https://www.linkedin.com/in/julian-kaiser-5b849519a/) 
+
+## FAQ
+### The Document model button does not appear in Power BI Desktop
+There are a few things that can cause this issue. There are a few things you can check up front:
+
+1. Do you run the latest version of Power BI Desktop? If not, please update first.
+2. Do you have other External Tools, such as DAX Studio, Tabular Editor or ALM Toolkit successfully running?
+3. Did you put the export2dataflow.pbitool.json file in the correct location? Please check the blogpost to find the correct location.
+
+If still nothing happens, or you don't have the External Tools folder on your PC, I advise you to install any of the above mentioned external tools that will generate this location for you during installation.
+
+### The buttons in the External Tools section in Power BI Desktop are greyed out
+External tools require Enhanced Meta Data to be enabled. You can enable this in the preview features of Power BI Desktop.
+
+1. Open Power BI Desktop
+2. Go to File
+3. Options & Settings
+4. Options
+5. In the Global Settings go to Preview Features
+6. Close all Power BI Desktop instances and re-open Power BI.
+
+###I clicked the button, saw the PowerShell window flickering on the screen, but nothing happened
+Most likely, this is caused by the execution policies for PowerShell configured on the computer. As far as I know, this is not something I can change in my script, but has everything to do with the setup of your PC or how your company configured it. This results in the fact that PowerShell.exe application did start, but it did not execute the script, because this was prevented by the policy. The solution to fix this, is changing the execution policy (which is a register thing on your computer. Please know, that this is all at your own risk! I cannot take any responsibility for this.
+
+The following steps might help you
+
+1. Open PowerShell.exe manually
+2. Execute Get-ExecutionPolicy -List
+3. As a result, you see the current configuration of your execution policies. Most probably everything is set to undefined at this moment.
+4. The easiest way to fix this, is by changing the execution policy for the current user. The ensures that this does not happen again. We need to do this by setting it to Unrestricted by executing this task: Set-ExecutionPolicy Unrestricted -Scope CurrentUser (all on your own responsibility and risk!)
+5. Confirm that you want to change this.
+6. Close PowerShell and Power BI Desktop
+Try again if it works now. More information about Execution Policies can be found [in this article](https://winaero.com/change-powershell-execution-policy-windows-10/)
+
+## License
+All code in this repository is licenses as specified by the [LICENSE](https://github.com/MarcusWegener/Export2Dataflow/blob/main/LICENSE) file.
